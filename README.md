b586469221ca42079c548904b585005f6c88dd28
jonbeau <jon.beaulieu@gmail.com>
2023-07-31 10:23:32 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# quart-template
+---
+title: Quart
+description: Quart is an asyncio reimplementation of the popular Flask microframework API.
+tags:
+  - python
+  - quart
+---
+
+# Python Quart Example
+
+This is a [Quart](https://quart.palletsprojects.com/en/latest/) app that serves a simple "Hello World" page.
+
+<!-- [![Deploy on Railway](https://railway.app/button.svg)](link) -->
+
+## ✨ Features
+
+- Python
+- Quart
+
+## 💁‍♀️ How to use
+
+- Install Python requirements `pip install -r requirements.txt`
---
+++ main.py
@@ -0,0 +1,33 @@
+
+import asyncio, os
+from routes.utils import app
+from quart import Quart, render_template
+from quart_compress import Compress
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+
+
+PORT =                  os.getenv('PORT')
+
+app = Quart(__name__, static_url_path='/static')
+Compress(app)
+
+
+@app.errorhandler(404)
+async def handle_not_found(e):
+    return '<h1>😦</h1><b>404</b> Not found.<p><a href="/">return</a>'
+
+
+@app.route("/")
+async def index() -> str:
+        return await render_template("index.html")
+
+
+if __name__ == "__main__":
+
+    app.run("0.0.0.0",PORT) # debug server for testing
+
+    ### uncomment for production
+    # config = Config()
+    # config.bind = [f"0.0.0.0:{PORT}"]
+    # asyncio.run(serve(app, config))
