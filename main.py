90f0da250d0f3b3a748207081c16ade51b3a51a8
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-06-28 01:17:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -168,7 +168,8 @@ async def process_url():
     id = data.get('id')
     print(data, url, id, TELETOKEN)
     result = await send_animation_url(TELETOKEN, id, file_url)
-    print(result.message_id)
+    parsable_result = result.json()
+    print(parsable_result.result.message_id)
     vision = await handle_img_link(url)
     print(vision)
 
