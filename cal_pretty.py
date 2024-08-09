d3e95a6c140d2a2bfb61a5c7660ebc357c2ae8df
HollowTwat <139015993+HollowTwat@users.noreply.github.com>
2024-08-09 21:16:43 +0300

    Update cal_pretty.py wiith detailed_format bool

--- cal_pretty.py
+++ cal_pretty.py
@@ -1,39 +1,39 @@
 import json
 
-
-async def prettify_and_count(data):
+async def prettify_and_count(data, detailed_format=True):
     json_data = json.loads(data)
-
+    
     if not json_data.get("food", []):
         json_data["pretty"] = "Не могу найти еду"
         resulting_json = json.dumps(json_data, ensure_ascii=False, indent=2)
         print(resulting_json)
         return resulting_json
-
+    
     pretty_list = []
 
-    for item in json_data["food"]:
-        nutritional_value = item["nutritional_value"]
-        fats = round(nutritional_value["fats"], 1)
-        carbs = round(nutritional_value["carbs"], 1)
-        protein = round(nutritional_value["protein"], 1)
+    if detailed_format:
+        for item in json_data["food"]:
+            nutritional_value = item["nutritional_value"]
+            fats = round(nutritional_value["fats"], 1)
+            carbs = round(nutritional_value["carbs"], 1)
+            protein = round(nutritional_value["protein"], 1)
 
-        # Calculate kcal
-        kcal = round(fats * 9 + carbs * 4 + protein * 4, 1)
-        nutritional_value["kcal"] = kcal
+            kcal = round(fats * 9 + carbs * 4 + protein * 4, 1)
+            nutritional_value["kcal"] = kcal
 
-        # Create pretty string
-        pretty_str = f"{item['description']} {item['weight']}г - {kcal} ккал ({fats}г жиров {carbs}г углеводов {protein}г белков)"
-        pretty_list.append(pretty_str)
+            pretty_str = f"{item['description']} {item['weight']}г - {kcal} ккал ({fats}г жиров {carbs}г углеводов {protein}г белков)"
+            pretty_list.append(pretty_str)
+        
+        pretty_output = "\n".join([f"{i+1}) {item}" for i, item in enumerate(pretty_list)])
+    else:
+        for item in json_data["food"]:
+            pretty_str = f"{item['description']} — {item['weight']} г"
+            pretty_list.append(pretty_str)
+        
+        pretty_output = "<b>Прием пищи:</b>\n\n" + "\n".join([f"{i+1}. {item}" for i, item in enumerate(pretty_list)])
 
-    # Join pretty list into one string
-    pretty_output = "\n".join([f"{i+1}) {item}" for i,
-                               item in enumerate(pretty_list)])
-
-    # Add pretty string to data
     json_data["pretty"] = pretty_output
 
-    # Print or save the resulting JSON
     resulting_json = json.dumps(json_data, ensure_ascii=False, indent=2)
     print(resulting_json)
     return resulting_json
